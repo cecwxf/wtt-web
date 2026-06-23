9040a20fc9dce6c89b76a0ceafff12daf0d0b547
cecwxf <cecwxf@126.com>
2026-06-23 16:08:18 +0800

    Publish Android APK 1.2.3 releases

--- lib/android-release.ts
+++ lib/android-release.ts
@@ -1,6 +1,6 @@
-export const ANDROID_LATEST_VERSION = "1.2.2";
-export const ANDROID_LATEST_VERSION_CODE = 14;
+export const ANDROID_LATEST_VERSION = "1.2.3";
+export const ANDROID_LATEST_VERSION_CODE = 15;
 export const ANDROID_LATEST_ABI = "universal";
-export const ANDROID_APK_DOWNLOAD_URL = "/downloads/wtt-android-v1.2.2-universal.apk";
+export const ANDROID_APK_DOWNLOAD_URL = "/downloads/wtt-android-v1.2.3-universal.apk";
 
 export const ANDROID_LATEST_LABEL = `v${ANDROID_LATEST_VERSION}`;
