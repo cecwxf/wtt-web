f46bab08e5e437e9be35c6b590842a73e85c4e36
cecwxf <cecwxf@126.com>
2026-06-28 21:00:23 +0800

    Update Android APK downloads

--- lib/android-release.ts
+++ lib/android-release.ts
@@ -1,6 +1,6 @@
-export const ANDROID_LATEST_VERSION = "1.2.3";
-export const ANDROID_LATEST_VERSION_CODE = 15;
+export const ANDROID_LATEST_VERSION = "1.2.9";
+export const ANDROID_LATEST_VERSION_CODE = 21;
 export const ANDROID_LATEST_ABI = "universal";
-export const ANDROID_APK_DOWNLOAD_URL = "/downloads/wtt-android-v1.2.3-universal.apk";
+export const ANDROID_APK_DOWNLOAD_URL = "/downloads/wtt-android-v1.2.9-universal.apk";
 
 export const ANDROID_LATEST_LABEL = `v${ANDROID_LATEST_VERSION}`;
